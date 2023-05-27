c92c4ddae15f3d1e902af3c2f0b23c41ec0c5773
lojaya <iksandi@lojaya.com>
2023-05-27 10:25:44 +0700

    feat: add footer

---
+++ components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="antialiased bg-white flex justify-center text-xs">
+    <div class="w-full my-6 max-w-2xl">
+      <div class="px-6 flex justify-between leading-normal">
+        <div class="flex flex-wrap flex-row justify-center">
+          <img src="/assets/ic-ngopijs.svg" width="42px" />
+          <div class="flex flex-wrap flex-col justify-center ml-2">
+            <p>2023 PT Kopi Jaya Digital</p>
+            <p>NgopiJS - Coffee & Code</p>
+          </div>
+        </div>
+        <div class="flex flex-wrap content-center">
+          <a class="ml-2" href="https://instagram.com/ngopi.js" target="_blank"><img src="/assets/ic-instagram.svg"
+              width="16px" /></a>
+          <a class="ml-2" href="https://github.com/ngopijs" target="_blank"><img src="/assets/ic-github.svg"
+              width="16px" /></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
